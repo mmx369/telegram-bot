337af8b3d6615132080f3b5ef971075fc96215ea
mmx369 <maksim@klishevich.com>
2022-10-06 23:28:07 +0300

    bot

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,35 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import TelegramApi from 'node-telegram-bot-api'
+import { againOptions, gameOptions } from './options'
 
 const token = process.env.TELEGRAM_TOKEN as string
 
 const bot = new TelegramApi(token, { polling: true })
 
-bot.setMyCommands([
-  { command: '/start', description: 'Начальное приветствие' },
-  { command: '/info', description: 'Информация о пользователе' },
-  { command: '/game', description: 'Угадайка' },
-])
-
 const chats = {} as any
 
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: 'Текст кнопки', callback_data: 'dasdsa' }]],
-  }),
+const startGame = async (chatId: number) => {
+  await bot.sendMessage(
+    chatId,
+    `Давай я загадаю число от 0 до 9. Сможешь отгадать? :)`
+  )
+  const randomNumber = Math.floor(Math.random() * 10)
+  chats[chatId] = randomNumber
+  await bot.sendMessage(chatId, 'Давай отгадывай!', gameOptions as any)
 }
 
 const start = () => {
+  bot.setMyCommands([
+    { command: '/start', description: 'Начальное приветствие' },
+    { command: '/info', description: 'Информация о пользователе' },
+    { command: '/game', description: 'Угадайка' },
+  ])
+
   bot.on('message', async (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
+
     if (text === '/start') {
       await bot.sendSticker(
         chatId,
@@ -40,19 +46,42 @@ const start = () => {
     }
 
     if (text === '/game') {
-      await bot.sendMessage(
-        chatId,
-        `Давай я загадаю число от 0 до 9. Сможешь отгадать? :)`
-      )
-      const randomNumber = Math.floor(Math.random() * 10)
-      chats[chatId] = randomNumber
-      return bot.sendMessage(chatId, 'Давай отгадывай!', gameOptions as any)
+      return startGame(chatId)
     }
 
     return bot.sendMessage(
       chatId,
-      `Привет, красавчик! Ты написал мне какую-то хрень - ${text} :(`
+      `Привет, красавчик! Ты написал мне какую-то хрень :(`
     )
+  })
+
+  bot.on('callback_query', async (msg) => {
+    const data = msg.data
+    const chatId = msg.message?.chat.id!
+
+    if (data === '/again') {
+      return startGame(chatId)
+    }
+
+    if (data === chats[chatId].toString()) {
+      return (
+        await bot.sendSticker(
+          chatId,
+          `https://tlgrm.eu/_/stickers/4ea/1cd/4ea1cd7d-6b79-3e89-a718-0e83b9f04749/9.webp`
+        ),
+        await bot.sendMessage(
+          chatId,
+          `Ты крут! Отгадал! Это действительно число ${data}.`
+        ),
+        againOptions
+      )
+    } else {
+      return bot.sendMessage(
+        chatId,
+        `Лузер! Я загадала число ${chats[chatId]}`,
+        againOptions
+      )
+    }
   })
 }
 
